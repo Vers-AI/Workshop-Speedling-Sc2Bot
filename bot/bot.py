87138c3f7534ae37bb9499bd1d735d719d77687a
Drekken <drekken.pownz@protonmail.com>
2024-12-09 17:45:20 -0500

    created a zerg rush bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,15 @@
 from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
 
+
+from sc2.ids.unit_typeid import UnitTypeId
+
+
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
     """This bot's name"""
 
-    RACE: Race = Race.Terran
+    RACE: Race = Race.Zerg
     """This bot's Starcraft 2 race.
     Options are:
         Race.Terran
@@ -13,7 +17,6 @@ class CompetitiveBot(BotAI):
         Race.Protoss
         Race.Random
     """
-
     async def on_start(self):
         """
         This code runs once at the start of the game
@@ -21,17 +24,45 @@ class CompetitiveBot(BotAI):
         """
         print("Game started")
 
-    async def on_step(self, iteration: int):
-        """
-        This code runs continually throughout the game
-        Populate this function with whatever your bot should do!
-        """
-        
-        pass
 
-    async def on_end(self, result: Result):
-        """
-        This code runs once at the end of the game
-        Do things here after the game ends
-        """
-        print("Game ended.")
+    async def on_step(self, iteration: int):
+        
+        if self.supply_used >= 12:
+            if (
+                    self.structures(
+                        UnitTypeId.SPAWNINGPOOL).amount
+                    + self.already_pending(
+                        UnitTypeId.SPAWNINGPOOL)
+                    == 0
+                ):
+            
+                if self.can_afford(
+                    UnitTypeId.SPAWNINGPOOL
+                    ):    
+                    await self.build(
+                        UnitTypeId.SPAWNINGPOOL, 
+                        near=self.townhalls.first
+                    )
+
+       
+        if self.structures(
+            UnitTypeId.SPAWNINGPOOL
+            ).ready and self.larva:
+            if self.can_afford(UnitTypeId.ZERGLING):
+                self.train(
+                    UnitTypeId.ZERGLING, 
+                    self.larva.amount)
+
+        if self.units(UnitTypeId.ZERGLING).ready:
+            for zergling in self.units(
+                UnitTypeId.ZERGLING
+                ):
+                zergling.attack(
+                    self.enemy_start_locations[0]
+                    )
+
+
+
+
+
+
